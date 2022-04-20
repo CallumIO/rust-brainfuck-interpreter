adbf716ecc5330ecdd4f22d30b3f7abc7f3474ea
Callum Leslie <github@cleslie.uk>
2022-04-20 21:38:16 +0100

    Create enumerator for all tokens in Brainfuck

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,26 @@
+// Enumerator for the different tokens in Brainfuck
+#[derive(PartialEq, Eq)]
+enum Token {
+    Inc,
+    Dec,
+    PtrR,
+    PtrL,
+    In,
+    Out,
+    SLoop,
+    ELoop,
+    Ignore,
+}
+
+#[derive(PartialEq, Eq)]
+enum Command {
+    Inc,
+    Dec,
+    PtrR,
+    PtrL,
+    In,
+    Out,
+    Loop(Vec<Command>),
+}
+
 fn main() {}
